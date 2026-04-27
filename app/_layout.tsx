5ac036b849a099672696e35c1c46e62ad302c6cc
monochandan <monochandan95@gmail.com>
2026-04-27 21:00:02 +0200

    custome auth provider

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,22 +1,22 @@
 import { Stack } from "expo-router";
 import { AuthProvider } from "./providers/AuthProviders";
-import { ClerkProvider } from '@clerk/expo'
-import { tokenCache } from '@clerk/expo/token-cache'
+// import { ClerkProvider } from '@clerk/expo'
+// import { tokenCache } from '@clerk/expo/token-cache'
 
-const publishablekey = process.env.EXPO_PUBLIC_CLERK_PUBLISHABLE_KEY!
+// const publishablekey = process.env.EXPO_PUBLIC_CLERK_PUBLISHABLE_KEY!
 
-if(!publishablekey){
-  throw new Error('Add your Clerk Publishable Key to the .env file');
-}
+// if(!publishablekey){
+//   throw new Error('Add your Clerk Publishable Key to the .env file');
+// }
 export default function RootLayout() {
   // console.log("publishable key: ", publishablekey);
   console.log("Root Layout")
   return(
 
      <AuthProvider>
-      <ClerkProvider publishableKey={publishablekey} tokenCache={tokenCache}>
+      {/* <ClerkProvider publishableKey={publishablekey} tokenCache={tokenCache}> */}
           <Stack screenOptions={{headerShown: false}}/>
-      </ClerkProvider>   
+      {/* </ClerkProvider>    */}
     </AuthProvider>
   )
 }
